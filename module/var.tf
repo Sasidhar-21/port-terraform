5172502073e513192dd80cde3c7a67d425f58e64
Sasidhar-21 <140780358+Sasidhar-21@users.noreply.github.com>
2024-06-13 12:26:02 +0530

    Create var.tf

---
+++ module/var.tf
@@ -0,0 +1,22 @@
+variable "env" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+variable "vpc_name" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+variable "project" {}
+# variable "ec2_ami" {
+#   default = "ami-0fc5d935ebf8bc3bc"
+# }
+# variable "key_name" {}
+# variable "instance_type" {
+#   type = string
+# }
