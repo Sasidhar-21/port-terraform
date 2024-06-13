dc3088942f27008e9d699d686d2cea164eaa8057
Sasidhar-21 <140780358+Sasidhar-21@users.noreply.github.com>
2024-06-13 13:25:02 +0530

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,27 +1,103 @@
-variable "vpc_name" {
-  description = "The name of the VPC"
-  type        = string
-}
-
-variable "vpc_cidr" {
-  description = "The CIDR block for the VPC"
-  type        = string
-}
-
-variable "region" {
-  description = "The AWS region to create the VPC in"
-  type        = string
-}
-
 provider "aws" {
   region = var.region
 }
 
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags for the resources"
+  type        = map(string)
+  default     = {}
+}
+
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
-  tags = {
-    Name = var.vpc_name
-  }
+
+  tags = merge(
+    var.tags,
+    {
+      Name = var.vpc_name
+    }
+  )
+}
+
+resource "aws_subnet" "public" {
+  count = 2
+  vpc_id     = aws_vpc.main.id
+  cidr_block = cidrsubnet(var.vpc_cidr, 8, count.index)
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.vpc_name}-public-${count.index}"
+    }
+  )
+}
+
+resource "aws_subnet" "private" {
+  count = 2
+  vpc_id     = aws_vpc.main.id
+  cidr_block = cidrsubnet(var.vpc_cidr, 8, count.index + 2)
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.vpc_name}-private-${count.index}"
+    }
+  )
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.vpc_name}-main"
+    }
+  )
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.vpc_name}-igw"
+    }
+  )
+}
+
+resource "aws_nat_gateway" "main" {
+  count = 2
+  subnet_id     = aws_subnet.public[count.index].id
+  allocation_id = aws_eip.nat[count.index].id
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.vpc_name}-nat-${count.index}"
+    }
+  )
+}
+
+resource "aws_eip" "nat" {
+  count = 2
+  vpc = true
 }
 
 output "vpc_id" {
@@ -41,7 +117,7 @@ output "private_subnet_ids" {
 }
 
 output "route_table_ids" {
-  value = aws_route_table.main.*.id
+  value = aws_route_table.main.id
 }
 
 output "internet_gateway_id" {
