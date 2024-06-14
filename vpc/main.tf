4623ca6f5378bb8216b72b7d474d5aaab01a4fbb
Sasidhar-21 <140780358+Sasidhar-21@users.noreply.github.com>
2024-06-14 13:06:41 +0530

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -35,61 +35,13 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public" {
-  count = 2
   vpc_id     = aws_vpc.main.id
-  cidr_block = cidrsubnet(var.vpc_cidr, 8, count.index)
+  cidr_block = cidrsubnet(var.vpc_cidr, 8, 0)
 
   tags = merge(
     var.tags,
     {
-      Name = "${var.vpc_name}-public-${count.index}"
-    }
-  )
-}
-
-resource "aws_subnet" "private" {
-  count = 2
-  vpc_id     = aws_vpc.main.id
-  cidr_block = cidrsubnet(var.vpc_cidr, 8, count.index + 2)
-
-  tags = merge(
-    var.tags,
-    {
-      Name = "${var.vpc_name}-private-${count.index}"
-    }
-  )
-}
-
-resource "aws_route_table" "main" {
-  vpc_id = aws_vpc.main.id
-
-  tags = merge(
-    var.tags,
-    {
-      Name = "${var.vpc_name}-main"
-    }
-  )
-}
-
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-
-  tags = merge(
-    var.tags,
-    {
-      Name = "${var.vpc_name}-igw"
-    }
-  )
-}
-
-resource "aws_nat_gateway" "main" {
-  count = 2
-  subnet_id = aws_subnet.public[count.index].id
-
-  tags = merge(
-    var.tags,
-    {
-      Name = "${var.vpc_name}-nat-${count.index}"
+      Name = "${var.vpc_name}-public"
     }
   )
 }
@@ -102,24 +54,8 @@ output "region" {
   value = var.region
 }
 
-output "public_subnet_ids" {
-  value = aws_subnet.public.*.id
-}
-
-output "private_subnet_ids" {
-  value = aws_subnet.private.*.id
-}
-
-output "route_table_ids" {
-  value = aws_route_table.main.id
-}
-
-output "internet_gateway_id" {
-  value = aws_internet_gateway.main.id
-}
-
-output "nat_gateway_ids" {
-  value = aws_nat_gateway.main.*.id
+output "public_subnet_id" {
+  value = aws_subnet.public.id
 }
 
 output "tags" {
