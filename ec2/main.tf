28c25054ad4a40eae49403de80545f411a407198
Sasidhar-21 <140780358+Sasidhar-21@users.noreply.github.com>
2024-06-14 16:21:48 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,27 +1,49 @@
+data "aws_vpc" "selected" {
+  filter {
+    name   = "tag:Name"
+    values = [var.vpc_name]
+  }
+}
+
+data "aws_subnet" "selected" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.selected.id]
+  }
+  
+  filter {
+    name   = "tag:Name"
+    values = ["public"]
+  }
+  
+  most_recent = true
+}
+
 data "aws_ami" "ubuntu" {
-    most_recent = true
+  most_recent = true
 
-    filter {
-        name   = "name"
-        values = ["ubuntu/images/hvm-ssd/*20.04-amd64-server-*"]
-    }
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/*20.04-amd64-server-*"]
+  }
 
-    filter {
-        name   = "virtualization-type"
-        values = ["hvm"]
-    }
-    
-    owners = ["099720109477"] # Canonical
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
 
 provider "aws" {
-  region  = var.aws_region
+  region = var.aws_region
 }
 
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.ec2_instance_type
   key_name      = var.pem_key_name
+  subnet_id     = data.aws_subnet.selected.id
 
   tags = {
     Name = var.ec2_name
